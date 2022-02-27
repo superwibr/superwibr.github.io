381ba22784b651b466ad438e15622df7556387b1
superwibr <superwibr@gmail.com>
2022-02-27 09:34:54 -0500

    possible milti-artist split fix

--- bml_src/monstercapture.js
+++ bml_src/monstercapture.js
@@ -34,6 +34,7 @@
 		const writer = new ID3Writer(mpfile);
 		writer.setFrame('TIT2', name)
 			.setFrame('TPE1', artists)
+			.setFrame('TPE2', release.ArtistsTitle)
 			.setFrame('TALB', albumName)
 			.setFrame('TRCK', track.TrackNumber)
 			.setFrame('TCON', [track.GenrePrimary, track.GenreSecondary])

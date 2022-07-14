e780e4cfd7a027f11daf0079613a3f803c8be0fb
superwibr <superwibr@gmail.com>
2022-07-14 11:39:26 -0400

    everything else

--- bml_src/but_why.js
+++ bml_src/but_why.js
@@ -1,27 +1,32 @@
 document.head.insertAdjacentHTML('beforeend', `\
-	* {
-		opacity: 0.0;
+	<style>
+	body * {
 		transition: opacity 1s;
 	}
-
-	* .inview {
-		opacity: 1;
-	}
+	</style>
 `)
 
 function inView(el) {
-	var sb = document.body.getBoundingClientRect();
-	var eb = el.getBoundingClientRect();
-	return !((eb.top + eb.height < 0) || (eb.top > sb.height));
+
+	const rect = el.getBoundingClientRect();
+	// DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+	const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+	const windowWidth = (window.innerWidth || document.documentElement.clientWidth);
+
+	// http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+	const vertInView = (rect.top <= windowHeight) && ((rect.top + rect.height) >= 0);
+	const horInView = (rect.left <= windowWidth) && ((rect.left + rect.width) >= 0);
+
+	return (vertInView && horInView);
 }
 
 function updateInView() {
-	for (x of document.querySelectorAll('*')) {
-		if (inView(x)) x.classList.add('inview')
-		else x.classList.remove('inview');
+	for (x of document.querySelectorAll('body *')) {
+		if (inView(x)) x.style.opacity = 1
+		else x.style.opacity = 0
 	}
 }
 
-document.body.onscroll = updateInView;
+document.onscroll = updateInView;
 
 updateInView();

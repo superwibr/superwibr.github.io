f5ca662f9649ce933df092c3dd0ba8f69466067f
superwibr <superwibr@gmail.com>
2023-01-10 06:52:26 -0500

    basic placeholder routing

--- src/n.js
+++ src/n.js
@@ -4,5 +4,18 @@
 ////////////////////////////////
 
 var n = {
-    
-}
+    navigate(path){
+        const qparams = new URLSearchParams(location.search),
+            origin = qparams.get("origin") ? decodeURI(qparams.get("origin")) : location.origin,
+            uri = origin + path;
+        
+        fetch(uri).then(res => res.text()).then(txt => document.querySelector(".ns-body > iframe").contentWindow.document.write(txt));
+    }
+};
+
+(function(){
+    const nhashnav = () => n.navigate(location.hash.substring(1, location.hash.length));
+
+    window.addEventListener("hashchange", nhashnav);
+    window.addEventListener("load", nhashnav);
+})();

3d7e65447d70355a369e3ede45e68b5684120083
superwibr <superwibr@gmail.com>
2023-01-29 22:30:27 -0500

    Navigation complete!

--- index.md
+++ index.md
@@ -7,7 +7,7 @@ The latest and greatest article-type viewer with all the features you don't need
 ### Projects
 - [Shadow boi animation](./sba/index.md)
 - [Bookmarklets](./bml.md)
-- [Stories, Universes, and Stuff](./n.html#/universe/index.html)
+- [Stories, Universes, and Stuff](./universe)
 - [Farre](./universe/farre/index.md)
 <br/><br/>
 
--- src/n.js
+++ src/n.js
@@ -17,17 +17,34 @@ var n = {
             iframe.style.display = "none";
             body1.style.display = "block"
             body1.innerHTML = content;
+            n.convertAnchors();
         } else {
             iframe.style.display = "block";
             body1.style.display = "none";
             ifdoc.open();
             ifdoc.write(content);
         }
+    },
+
+    async convertAnchors() {
+        for (const anchor of document.querySelectorAll("a")) {
+            console.log(anchor.getAttribute("href"))
+            // only applies to relative urls. Basic test if starts with a period or a slash
+            if (anchor.getAttribute("href").match(/^[.\/]/)) {
+                const path = await n.parsePath(anchor.getAttribute("href"));
+                console.log(path)
+                anchor.href = "#" + path;
+            };
+        }
+    },
+    async parsePath(path) {
+        const url = await fetch(location.origin + location.hash.substring(1), {method:"HEAD"}).then(res => res.url);
+        return new URL(path, url).href.substring(location.origin.length);
     }
 };
 
 (function () {
-    const nhashnav = () => n.navigate(location.hash.substring(1, location.hash.length));
+    const nhashnav = () => n.navigate(location.hash.substring(1));
 
     window.addEventListener("hashchange", nhashnav);
     window.addEventListener("load", nhashnav);

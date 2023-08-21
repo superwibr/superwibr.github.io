dff2681ad00bedc89bba72810485793d2a26befd
superwibr <superwibr@gmail.com>
2023-08-21 14:14:50 -0400

    tabs

--- bml_src/scene-exe/ddmods/doshen.js
+++ bml_src/scene-exe/ddmods/doshen.js
@@ -1,68 +1,68 @@
 !function () {
-    // debug
-    const log = message => console.log(`%c[DOSHEN] %c${message}`, `color:goldenrod;font-weight:bold`, "");
+	// debug
+	const log = message => console.log(`%c[DOSHEN] %c${message}`, `color:goldenrod;font-weight:bold`, "");
 
-    // init objects
-    if (typeof exe === 'object') log("[WARN] `exe` global already defined, some issues may arise if you didn't stop the previous instance.");
-    window.exe = new Object();
-    window._exe = new Object();
-    _exe.log = log;
-    log("Initialized! Module loader accessible from `exe.mods`");
+	// init objects
+	if (typeof exe === 'object') log("[WARN] `exe` global already defined, some issues may arise if you didn't stop the previous instance.");
+	window.exe = new Object();
+	window._exe = new Object();
+	_exe.log = log;
+	log("Initialized! Module loader accessible from `exe.mods`");
 
 
-    // loader
-    exe.mods = {
-        _modules: {
-            msgpackr: {
-                env: "global",
-                uri: "./bml_src/msgpackr.js"
-            },
-            fovmod: {
-                env: "global",
-                uri: "./bml_src/scene-exe/fovmod.js"
-            },
-            hidsim: {
-                env: "global",
-                name: "hidsim",
-                uri: "./bml_src/hidsim.js"
-            },
-            packetd: {
-                env: "module",
-                name: "pakd",
-                uri: "./bml_src/scene-exe/ddmods/packetd.js"
-            }
-        },
-        load: async function (modname) {
-            const modEntry = this._modules[modname];
+	// loader
+	exe.mods = {
+		_modules: {
+			msgpackr: {
+				env: "global",
+				uri: "./bml_src/msgpackr.js"
+			},
+			fovmod: {
+				env: "global",
+				uri: "./bml_src/scene-exe/fovmod.js"
+			},
+			hidsim: {
+				env: "global",
+				name: "hidsim",
+				uri: "./bml_src/hidsim.js"
+			},
+			packetd: {
+				env: "module",
+				name: "pakd",
+				uri: "./bml_src/scene-exe/ddmods/packetd.js"
+			}
+		},
+		load: async function (modname) {
+			const modEntry = this._modules[modname];
 
-            if (!modEntry) return log(`[ERR] module "${modname}" not found`);
-            if (modEntry.loaded) return; // stop from loading already loaded modules
+			if (!modEntry) return log(`[ERR] module "${modname}" not found`);
+			if (modEntry.loaded) return; // stop from loading already loaded modules
 
-            const mod = await fetch("https://superwibr.github.io/" + modEntry.uri).then(res => res.text());
+			const mod = await fetch("https://superwibr.github.io/" + modEntry.uri).then(res => res.text());
 
-            switch (modEntry.env) {
-                case "global": // global scope eval for modules which define themselves
-                    (0, eval)(mod);
-                    break;
+			switch (modEntry.env) {
+				case "global": // global scope eval for modules which define themselves
+					(0, eval)(mod);
+					break;
 
-                case "local": // local eval and attachment for independant modules
-                    exe[modEntry.name] = await eval(mod);
-                    break;
+				case "local": // local eval and attachment for independant modules
+					exe[modEntry.name] = await eval(mod);
+					break;
 
-                case "module": // same as local but for those who don't self-instantiate and require other modules
-                    exe[modEntry.name] = await eval(mod)(_exe, exe);
-                    break;
+				case "module": // same as local but for those who don't self-instantiate and require other modules
+					exe[modEntry.name] = await eval(mod)(_exe, exe);
+					break;
 
-                default:
-                    break;
-            };
+				default:
+					break;
+			};
 
-            modEntry.loaded = true; // tag loaded modules so we don't re-load them
-            log(`Loaded ${modname} ${(modEntry.env != "global" && modEntry.name) ? `at exe.${modEntry.name}` : ""}`);
-        },
-        loadall: async function(){
-            const mods = Object.keys(this._modules);
-            for (const mod of mods) await this.load(mod);
-        }
-    };
+			modEntry.loaded = true; // tag loaded modules so we don't re-load them
+			log(`Loaded ${modname} ${(modEntry.env != "global" && modEntry.name) ? `at exe.${modEntry.name}` : ""}`);
+		},
+		loadall: async function () {
+			const mods = Object.keys(this._modules);
+			for (const mod of mods) await this.load(mod);
+		}
+	};
 }();
